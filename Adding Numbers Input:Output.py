dbdfd0c726bd6d999a631e37acbf8b5ed8f368d4
hsmith851 <hsmith4665@gmail.com>
2020-07-31 17:15:20 -0400

    Add files via upload

---
+++ Adding Numbers Input:Output.py
@@ -0,0 +1,11 @@
+from numpy import double
+#program to add two numbers
+#Store input numbers
+from numpy import double
+
+num1 = input('Enter the first number: ')
+num2 = input('Enter the second number: ')
+# Adding two nos
+sum = double(num1) + double(num2)
+# printing values
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
